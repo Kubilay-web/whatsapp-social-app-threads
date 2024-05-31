23be3ab8834c071966ebddb509d5d1680903190d
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-05-31 16:20:48 +0300

    Updated

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+  getUserPosts,
+  getAllPosts,
+} from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/feed", protectRoute, getFeedPosts);
+router.get("/allPost", protectRoute, getAllPosts);
+router.get("/:id", getPost);
+router.get("/user/:username", getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.put("/like/:id", protectRoute, likeUnlikePost);
+router.put("/reply/:id", protectRoute, replyToPost);
+
+export default router;
